3500b389e4ba05e5b8d77a936a131debc499648c
yuri-mish <admin@starsam.net>
2021-02-11 11:07:36 +0200

    111

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,35 +1,36 @@
-import defaultUser from '../utils/default-user';
-import { API_HOST } from './../constants';
+import defaultUser from "../utils/default-user";
+import { API_HOST } from "./../constants";
 
-const getAuth = async (user,pass) => {
-  const auth =  await  fetch(API_HOST, {
+const getAuth = async (user, pass) => {
+  const auth = await fetch(API_HOST, {
     method: "POST",
-    credentials:'include', 
+    credentials: "include",
     //mode:'cors',
-      body: JSON.stringify({
-      query: `{auth (name:"${user}",pass:"${pass}") }`, 
+    body: JSON.stringify({
+      query: `{auth (name:"${user}",pass:"${pass}") }`,
     }),
     headers: {
       "Content-Type": "application/json",
     },
-    })
+  })
     .then((response) => {
-      console.log(response)
+      console.log(response);
       return response.json();
     })
     .then((data) => {
-//      data.data.auth.branch = data.data.branch
-      if (data.data.auth) data.data.auth.isOk = true 
-       return data.data.auth||{isOk:false}
-    }).catch((e)=>console.log('Ooops',e));
+      //      data.data.auth.branch = data.data.branch
+      if (data.data.auth) data.data.auth.isOk = true;
+      return data.data.auth || { isOk: false };
+    })
+    .catch((e) => console.log("Ooops", e));
 
-    return auth
+  return auth;
 };
 
-export const logout = async (user,pass) => {
+export const logout = async (user, pass) => {
   return await fetch(API_HOST, {
     method: "POST",
-    credentials:'include', 
+    credentials: "include",
     //credentials: 'same-origin',
     body: JSON.stringify({
       query: `{logout
@@ -38,56 +39,53 @@ export const logout = async (user,pass) => {
     headers: {
       "Content-Type": "application/json",
     },
-    })
+  })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-       return data.data.logout
+      return data.data.logout;
     });
 };
 
-
 export async function signIn(email, password) {
   try {
-    var result = await getAuth(email,password)
+    var result = await getAuth(email, password);
     // Send request
     //console.log(result, email, password);
 
     return {
       isOk: result.isOk,
-      message: result.isOk ? '':'Помилка входу',
+      message: result.isOk ? "" : "Помилка входу",
       data: {
-              email: email,
-              avatarUrl: 'https://otk.in.ua/wp-content/uploads/2020/07/logo.svg',
-}
+        email: email,
+        avatarUrl: "https://otk.in.ua/wp-content/uploads/2020/07/logo.svg",
+      },
     };
-  }
-  catch {
+  } catch {
     return {
       isOk: false,
-      message: "Authentication failed"
+      message: "Authentication failed",
     };
   }
 }
 
-export async function getUser() {
-  try {
-    // Send request
+// export async function getUser() {
+//   try {
+//     // Send request
 
-    return {
-      isOk: false,
+//     return {
+//       isOk: false,
 
-      //      isOk: true,
-//      data: defaultUser
-    };
-  }
-  catch {
-    return {
-      isOk: false
-    };
-  }
-}
+//       //      isOk: true,
+//       //      data: defaultUser
+//     };
+//   } catch {
+//     return {
+//       isOk: false,
+//     };
+//   }
+// }
 
 export async function createAccount(email, password) {
   try {
@@ -95,13 +93,12 @@ export async function createAccount(email, password) {
     console.log(email, password);
 
     return {
-      isOk: true
+      isOk: true,
     };
-  }
-  catch {
+  } catch {
     return {
       isOk: false,
-      message: "Failed to create account"
+      message: "Failed to create account",
     };
   }
 }
@@ -112,14 +109,13 @@ export async function changePassword(email, recoveryCode) {
     console.log(email, recoveryCode);
 
     return {
-      isOk: true
+      isOk: true,
     };
-  }
-  catch {
+  } catch {
     return {
       isOk: false,
-      message: "Failed to change password"
-    }
+      message: "Failed to change password",
+    };
   }
 }
 
@@ -129,13 +125,12 @@ export async function resetPassword(email) {
     console.log(email);
 
     return {
-      isOk: true
+      isOk: true,
     };
-  }
-  catch {
+  } catch {
     return {
       isOk: false,
-      message: "Failed to reset password"
+      message: "Failed to reset password",
     };
   }
 }
--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect, createContext, useContext, useCallback } from 'react';
-import { getUser, signIn as sendSignInRequest } from '../api/auth';
+import { signIn as sendSignInRequest } from '../api/auth';
 
 function AuthProvider(props) {
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    (async function () {
-      const result = await getUser();
-      if (result.isOk) {
-        setUser(result.data);
-      }
-
-      setLoading(false);
-    })();
+    // (async  () =>{
+      // const result = await getUser();
+      // if (result.isOk) {
+      //   setUser(result.data);
+      // }
+       setLoading(false);
+    //})();
   }, []);
 
   const signIn = useCallback(async (email, password) => {
